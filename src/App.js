44a8ebdd8bb527e285e7309b51b0226110f0f83d
KarthiSiva4 <karthisivatcs@gmail.com>
2022-07-23 15:31:24 +0530

    basic components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       Going to create a Saree Business app....
+      <Homepage />
     </div>
   );
 }
